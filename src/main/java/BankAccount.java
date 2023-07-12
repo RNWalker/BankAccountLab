8360855ef2539672176919b8408c7a601e3b0d2c
RNWalker <rebecca.noelia.walker@gmail.com>
2023-07-12 16:26:34 +0100

    testing getters and setters

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,10 +1,31 @@
 public class BankAccount {
 
-    private String = firstName;
-    private String = lastName;
-    private String = dateOfBirth
-    private int = accountNumber; //enum?
-    private int = balance;
+    private int accountNumber;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private int balance;
+
+    public BankAccount() {
+        this.firstName = "Rebecca";
+        this.lastName = "Walker";
+        this.dateOfBirth = "18th November 1998";
+        this.accountNumber = 1234; //enum?
+        this.balance = 0;
+    }
+
+    String getFirstName(){return this.firstName;}
+    String getLastName(){return this.lastName;}
+    String getDateOfBirth(){return this.dateOfBirth;}
+    int getAccountNumber(){return this.accountNumber;}
+    int getBalance(){return this.balance;}
+
+    public String setFirstName(){return this.firstName = "Noelia";}
+    public String setLastName(){return this.lastName = "Benson";}
+    public String setDateOfBirth(){return this.dateOfBirth = "18th November 2000";}
+    public int setAccountNumber(){return this.accountNumber = 5678;}
+    public int setBalance(){return this.balance = 12;}
+
 
 
 }
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,3 +1,78 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class BankAccountTest {
+
+    BankAccount bankAccount;
+    @BeforeEach
+    public void setUp(){
+        bankAccount = new BankAccount();
+    }
+
+    @Test
+    public void canGetFirstName(){
+        String result = bankAccount.getFirstName();
+        String expected = "Rebecca";
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void canGetLastName(){
+        String result = bankAccount.getLastName();
+        String expected = "Walker";
+        assertThat(result).isEqualTo(expected);
+    }
+@Test
+    public void canGetDateOfBirth(){
+        String result = bankAccount.getDateOfBirth();
+        String expected = "18th November 1998";
+        assertThat(result).isEqualTo(expected);
+    }
+@Test
+    public void canGetAccountNumber(){
+        int result = bankAccount.getAccountNumber();
+        int expected = 1234;
+        assertThat(result).isEqualTo(expected);
+    }
+@Test
+    public void canGetBalance(){
+        int result = bankAccount.getBalance();
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+@Test
+    public void canSetFirstName(){
+        String result = bankAccount.setFirstName();
+        String expected = "Noelia";
+    assertThat(result).isEqualTo(expected);
+}
+    @Test
+    public void canSetLastName(){
+        String result = bankAccount.setLastName();
+        String expected = "Benson";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void canSetDateOfBirth(){
+        String result = bankAccount.setDateOfBirth();
+        String expected = "18th November 2000";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void canSetAccountNumber(){
+        int result = bankAccount.setAccountNumber();
+        int expected = 5678;
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void canSetBalance(){
+        int result = bankAccount.setBalance();
+        int expected = 12;
+        assertThat(result).isEqualTo(expected);
+    }
+
 
 }
