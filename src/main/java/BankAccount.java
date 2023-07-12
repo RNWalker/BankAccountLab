1ec15f3d38f627623128ee8f024a358478abc4f1
RNWalker <rebecca.noelia.walker@gmail.com>
2023-07-12 15:45:42 +0100

    classes and testing

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,10 @@
+public class BankAccount {
+
+    private String = firstName;
+    private String = lastName;
+    private String = dateOfBirth
+    private int = accountNumber; //enum?
+    private int = balance;
+
+
+}
